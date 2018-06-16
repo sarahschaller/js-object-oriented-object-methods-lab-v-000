512429482f99c3f84278138717a219b030b451ec
Sarah Schaller <sarahcschaller@gmail.com>
2018-06-16 19:44:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ function BoardMember(name, homeState, training) {
   this.homeState = homeState;
   this.training = training;
   this.veto = function() {
-    return "No, I must disagree"
+    return "No, I must disagree";
   };
   this.approve = function() {
-    return "You can do that!"
+    return "You can do that!";
   };
+  this.doCharity = function() {
+    return "I like to help people."
+  }
   this.releasePressStatement = function() {
-    return "You will see great things from Scuber."
+    return "You will see great things from Scuber.";
   };
 }
