4b00348bf953a91a21d311736f59799296a64207
Sarah Schaller <sarahcschaller@gmail.com>
2018-06-16 19:44:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function BoardMember(name, homeState, training) {
   this.approve = function() {
     return "You can do that!"
   };
-  this.doCharity = function() {
+  this.releasePressStatement = function() {
     return "You will see great things from Scuber."
   };
 }
