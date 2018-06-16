aa3e826746a14cdab1e031c0aa33b881c7c4cded
Sarah Schaller <sarahcschaller@gmail.com>
2018-06-16 19:45:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ function BoardMember(name, homeState, training) {
     return "You can do that!";
   };
   this.doCharity = function() {
-    return "I like to help people."
-  }
+    return "I like to help people.";
+  };
   this.releasePressStatement = function() {
     return "You will see great things from Scuber.";
+  };
+  this.sayHi = function() {
+    return `Hi, my name is ${this.name}. I am from ${homeState}, and I was trained in ${training}.`;
   };
 }
